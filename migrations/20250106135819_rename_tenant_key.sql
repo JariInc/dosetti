34ad47cff6336f1ddd5112e5eb5cfda38951147b
Jari Ylimäinen <jari@ylimainen.fi>
2025-01-27 07:29:45 +0200

    Remove statements as they don't work when migration from go

--- migrations/20250106135819_rename_tenant_key.sql
+++ migrations/20250106135819_rename_tenant_key.sql
@@ -1,13 +1,7 @@
 -- +goose Up
--- +goose StatementBegin
 ALTER TABLE tenant
 DROP COLUMN name;
 
--- +goose StatementEnd
---
 -- +goose Down
--- +goose StatementBegin
 ALTER TABLE
 ADD COLUMN name TEXT NOT NULL;
-
--- +goose StatementEnd
